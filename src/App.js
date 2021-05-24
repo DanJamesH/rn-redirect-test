f36a17c97e2882c1a288900742190ccffcb4a373
Dan <daniel.hewlett@switch.org.za>
2021-05-24 17:47:08 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React, { useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  function openInNewTab(url) {
+    var win = window.open("https://reactjs.org", '_blank');
+    if (win != null) {
+      win.focus();
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => openInNewTab(),1000)
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and saveeee to reload.
         </p>
         <a
           className="App-link"
